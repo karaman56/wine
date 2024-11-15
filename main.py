2a443e0a0c65384b08ae702471646d811553d982
karaman56 <I@kapaman.ru>
2024-11-15 18:19:39 +0300

    работа

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+from datetime import datetime
+
+current_year = datetime.now().year
+start_year = 1920
+years_difference = current_year - start_year
+ending_data = str(years_difference)
+ending = int(ending_data[-1])
+if ending == 0 or (5 <= ending <= 9):
+    ending_year = 'лет'
+elif ending == 1:
+    ending_year = 'год'
+else:
+    ending_year = 'года'
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
 template = env.get_template('template.html')
-
 rendered_page = template.render(
-    cap1_title="Красная кепка",
-    cap1_text="$ 100.00",
-    cap2_title="Чёрная кепка",
-    cap2_text="$ 120.00",
-    cap3_title="Ещё одна чёрная кепка",
-    cap3_text="$ 90.00",
+    text1="ПРОВЕРЕНО ВРЕМЕНЕМ",
+    text2=(f"Уже {years_difference} {ending_year} с вами"),
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
