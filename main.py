b96745adc572938782c95407308342a622806967
karaman56 <166434324+karaman56@users.noreply.github.com>
2024-11-20 00:41:17 +0500

    загрузил код вебсайт вина
    Это веб-приложение позволяет пользователям просматривать коллекцию вин. Вы можете увидеть различные категории вин, такие как белые и красные, а также специальные предложения. На каждом вине отображается изображение, название, сорт и цена.

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
-
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-
+import pandas as pd
+from collections import defaultdict
 from datetime import datetime
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import os
 
-current_year = datetime.now().year
-start_year = 1920
-years_difference = current_year - start_year
-ending_data = str(years_difference)
-ending = int(ending_data[-1])
-if ending == 0 or (5 <= ending <= 9):
-    ending_year = 'лет'
-elif ending == 1:
-    ending_year = 'год'
-else:
-    ending_year = 'года'
+IMAGE_FOLDER = 'images/'
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def main():
+    excel_data_df = pd.read_excel(
+        'wine.xlsx',
+        sheet_name='wine_al',
+        usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция']
+    )
+    data_list = excel_data_df.to_dict(orient='records')
 
-template = env.get_template('template.html')
-rendered_page = template.render(
-    text1="ПРОВЕРЕНО ВРЕМЕНЕМ",
-    text2=(f"Уже {years_difference} {ending_year} с вами"),
-)
+    product_dict = defaultdict(list)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    for item in data_list:
+        item = {key: (value if pd.notna(value) else '') for key, value in item.items()}
+        item['Картинка'] = os.path.join(IMAGE_FOLDER, item['Картинка'])
+        category = item['Категория']
+        product_dict[category].append(item)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    current_year = datetime.now().year
+    start_year = 1920
+    years_difference = current_year - start_year
+    ending_data = str(years_difference)
+    ending = int(ending_data[-1])
+    if ending == 0 or (5 <= ending <= 9):
+        ending_year = 'лет'
+    elif ending == 1:
+        ending_year = 'год'
+    else:
+        ending_year = 'года'
 
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        text1="ПРОВЕРЕНО ВРЕМЕНЕМ",
+        text2=(f"Уже {years_difference} {ending_year} с вами"),
+        product_dict=product_dict
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    main()
 
--- xlll.py
+++
@@ -1,6 +0,0 @@
-import pandas
-
-excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='wine')
-
-# print whole sheet data
-print(excel_data_df)
